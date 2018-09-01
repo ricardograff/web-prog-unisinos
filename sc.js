059488263d2a8910399832477a264b2439201ceb
Ricardo Graff <tilt@MacBook-Pro-de-Ricardo.local>
2018-09-01 10:41:48 -0300

    added javascript

---
+++ sc.js
@@ -0,0 +1,12 @@
+var password = 0;
+var login = '';
+
+// Event Listener Btn Submit 
+
+document.getElementById("myBtn").addEventListener('click', function (){
+    console.log('Teste')
+    login = document.getElementById("login").value;
+    password = document.getElementById("pass").value;
+});
+
+
